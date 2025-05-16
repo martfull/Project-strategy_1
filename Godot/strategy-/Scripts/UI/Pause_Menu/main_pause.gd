701ca6097b846a596916731b09f7d36773fbf12e
Artem Nakonechnyi <artem12345556789@gmail.com>
2025-05-16 05:21:20 +0200

    Fix bugs pause menu

--- Godot/strategy-/Scripts/UI/Pause_Menu/main_pause.gd
+++ Godot/strategy-/Scripts/UI/Pause_Menu/main_pause.gd
@@ -1,21 +1,29 @@
 extends Node2D
+
+	
+	
 func _input(event):
 	if event.is_action_pressed("ui_cancel"):
 		if get_tree().paused: 
-			resume_game()
+			resume_game()		
 		else: pause_game()
 		
+		
 func resume_game():
+	get_tree().paused = false
 	$PauseMenu.visible = false 	
 	
+	
 func pause_game():
+	get_tree().paused = true
 	$PauseMenu.visible = true 	
 	
 	
-
 func _on_button_pressed() -> void:
 	resume_game()
 
 
 func _on_button_2_pressed() -> void:
-	get_tree().change_scene_to_file("B:/Satoru/Project-strategy/Godot/strategy-/Scenes/test_menu.tscn")
+	get_tree().paused = false
+	$PauseMenu.visible = false
+	get_tree().change_scene_to_file("res://Godot/strategy-/Scenes/test_menu.tscn")
