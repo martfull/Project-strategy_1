5b17be6e585a3abed5ff71f8cf9c53e517ee8f55
Artem Nakonechnyi <artem12345556789@gmail.com>
2025-05-20 22:27:47 +0200

    отмена супер мерджа(

--- Godot/strategy-/Scripts/C#/PlayerSprite.cs
+++ Godot/strategy-/Scripts/C#/PlayerSprite.cs
@@ -4,27 +4,27 @@ using Strategy.Scripts;
 
 public partial class PlayerSprite : Sprite2D
 {
-    [Export]
-    public int Width{get;set;}
-    [Export]
-    public int Height{get;set;}
-    public Vector2I Coord{get;set;}
-    public string Name{get;set;}
-    
-    
-    public void SetCoordinatePosition(int x, int y)
-    {
-        int TileX = x * Width;
-        int TileY = y * Height;
-        
-        Position = new Vector2I(TileX, TileY);
-        Coord = new Vector2I(x,y);
-    }
-    
-    public void MoveTo(TileSprite tile)
-    {
-        this.Coord = tile.Cood;
-        this.Position = tile.Position;
+	[Export]
+	public int Width{get;set;}
+	[Export]
+	public int Height{get;set;}
+	public Vector2I Coord{get;set;}
+	public string Name{get;set;}
+	
+	
+	public void SetCoordinatePosition(int x, int y)
+	{
+		int TileX = x * Width;
+		int TileY = y * Height;
+		
+		Position = new Vector2I(TileX, TileY);
+		Coord = new Vector2I(x,y);
+	}
+	
+	public void MoveTo(TileSprite tile)
+	{
+		this.Coord = tile.Cood;
+		this.Position = tile.Position;
 
-    }
+	}
 }
